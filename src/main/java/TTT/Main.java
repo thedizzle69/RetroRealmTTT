12ac36f2ee7b60d78ff8fff6de3ab880ca0affcc
thedizzle69 <jai.singh29192@gmail.com>
2023-12-05 23:34:52 +0100

    Created blank project with hello world example

--- src/main/java/TTT/Main.java
+++ src/main/java/TTT/Main.java
@@ -1,6 +1,6 @@
 package TTT;
-import java.util.Scanner;
 
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
@@ -10,11 +10,19 @@ public class Main {
         while (true) {
             game.printBoard();
 
-            System.out.print("row (0-2): ");
-            int row = scanner.nextInt();
-            System.out.print("column (0-2): ");
-            int col = scanner.nextInt();
+            try {
+                System.out.print("row (0-2): ");
+                int row = scanner.nextInt();
+
+                System.out.print("column (0-2): ");
+                int col = scanner.nextInt();
+
+                // Your logic for handling user input here
+
+            } catch (java.util.InputMismatchException e) {
+                System.out.println("Invalid input. Please enter a valid integer.");
+                scanner.nextLine(); // Consume the invalid input to avoid an infinite loop
+            }
         }
     }
 }
-
