e238b2a4d338150d27e903594c73e70d85981bf7
thedizzle69 <jai.singh29192@gmail.com>
2023-12-05 23:45:26 +0100

    Created blank project with hello world example

--- src/main/java/TTT/TTT.java
+++ src/main/java/TTT/TTT.java
@@ -1,37 +1,61 @@
 package TTT;
 
-/* import java.util.Scanner; */
+import java.util.Scanner;
 
 public class TTT {
-        char[][] board;
-        char currentPlayer;
+    char[][] board;
+    char currentPlayer;
 
-        public TTT() {
-            board = new char[3][3];
-            currentPlayer = 'X';
-            initializeBoard();
-        }
+    public TTT() {
+        board = new char[3][3];
+        currentPlayer = 'X';
+        initializeBoard();
+    }
 
-        public void initializeBoard() {
-            for (int i = 0; i < 3; i++) {
-                for (int j = 0; j < 3; j++) {
-                    board[i][j] = ' ';
-                }
+    public void initializeBoard() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                board[i][j] = ' ';
             }
-        }
-
-        public void printBoard() {
-            System.out.println("Current Player: " + currentPlayer);
-            System.out.println("▁▁▁▁▁▁");
-            for (int i = 0; i < 3; i++) {
-                System.out.print("|");
-                for (int j = 0; j < 3; j++) {
-                    System.out.print(board[i][j]);
-                    System.out.print("|");
-                }
-                System.out.println();
-            }
-            System.out.println("▔▔▔▔");
         }
     }
 
+    public void printBoard() {
+        System.out.println("Current Player: " + currentPlayer);
+        System.out.println("▁▁▁▁▁▁");
+        for (int i = 0; i < 3; i++) {
+            System.out.print("|");
+            for (int j = 0; j < 3; j++) {
+                System.out.print(board[i][j]);
+                System.out.print("|");
+            }
+            System.out.println();
+        }
+        System.out.println("▔▔▔▔");
+    }
+    boolean makeMove(int row, int col) {
+        if (row < 0 || row >= 3 || col < 0 || col >= 3 || board[row][col] != ' ') {
+            return false;
+        }
+
+        board[row][col] = currentPlayer;
+        return true;
+    }
+    boolean checkWin() {
+        // Check rows, columns, and diagonals for a win
+        for (int i = 0; i < 3; i++) {
+            if (board[i][0] == currentPlayer && board[i][1] == currentPlayer && board[i][2] == currentPlayer) {
+                return true; // Row win
+            }
+            if (board[0][i] == currentPlayer && board[1][i] == currentPlayer && board[2][i] == currentPlayer) {
+                return true; // Column win
+            }
+        }
+        if (board[0][0] == currentPlayer && board[1][1] == currentPlayer && board[2][2] == currentPlayer) {
+            return true; // Diagonal win
+        }
+        if (board[0][2] == currentPlayer && board[1][1] == currentPlayer && board[2][0] == currentPlayer) {
+            return true; // Diagonal win
+        }
+        return false;}
+}
