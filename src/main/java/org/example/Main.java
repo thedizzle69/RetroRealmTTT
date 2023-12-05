549be70f90e521b32f2942285a91ec7b8b5f916f
taomero <tahir_007@live.com>
2023-12-05 14:28:44 +0100

    The main menu has been added.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,20 @@
 package org.example;
+import java.util.Scanner;
+
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        TTT game = new TTT();
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            game.printBoard();
+
+            System.out.print("row (0-2): ");
+            int row = scanner.nextInt();
+            System.out.print("column (0-2): ");
+            int col = scanner.nextInt();
+        }
     }
 }
+
